cbd3abe1d9b3c18581737b2712e7c0da8a9c244b
Jchips <20979096+Jchips@users.noreply.github.com>
2023-05-02 20:59:18 -0700

    got get request working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      city: ''
+    }
+  }
+
+  updateCity = (e) => {
+    console.log(e.target.value); // delete later
+    this.setState({city: e.target.value})
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    let url =  `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.city}&format=json`;
+    const response =  await axios.get(url);
+    console.log(response.data);
+  }
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Label>City name</Form.Label>
+            <Form.Control onChange={this.updateCity} name="cityName" type="text" placeholder="enter city" />
+          </Form.Group>
+          <Button variant="primary" type="submit">Explore!</Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default App;
+
